66f6ee05835cdef1c9798aab30724a5b8dea3b00
Nurlan Purkhanov <ppurkhanov@gmail.com>
2024-07-22 01:16:30 +0500

    fixed bugs

--- src/database/base.py
+++ src/database/base.py
@@ -5,12 +5,12 @@ from config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-# engine = create_async_engine(DATABASE_URL)
-engine = create_async_engine(
-    url = "sqlite+aiosqlite:///./athkeeper.db",
-    connect_args = {"check_same_thread": False},
-    echo = True
-)
+engine = create_async_engine(DATABASE_URL)
+# engine = create_async_engine(
+#     url = "sqlite+aiosqlite:///./athkeeper.db",
+#     connect_args = {"check_same_thread": False},
+#     echo = True
+# )
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
