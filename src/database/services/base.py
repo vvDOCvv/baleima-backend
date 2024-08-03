d262b4631ceb8173fb837ed1721c5e1ac8bf0821
Nurlan Purkhanov <ppurkhanov@gmail.com>
2024-08-03 12:38:24 +0500

    fixed bugs

--- src/database/services/base.py
+++ src/database/services/base.py
@@ -2,6 +2,6 @@ from database.utils.unitofwork import IUnitOfWork
 
 
 class BaseService:
-    async def get_base_info(uow: IUnitOfWork):
+    async def get_base_info(self, uow: IUnitOfWork):
         async with uow:
             return await uow.base.get_user_and_trade_count_and_profit_sum()
